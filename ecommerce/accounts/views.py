ace6626cab98740720b8382b6bb709e225c469a9
unknown <yuktibajaj85@yahoo.com>
2021-03-15 15:30:52 +0530

    edits

--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth import login, logout, authenticate
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 # Create your views here.
 
 
@@ -16,6 +16,20 @@ def login_view(request):
         password = form.cleaned_data['password']
         user = authenticate(username=username, password=password)
         login(request, user)
+
+    context = {'form': form}
+    return render(request, 'form.html', context)
+
+
+def registration_view(request):
+    form = RegistrationForm(request.POST or None)
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        new_user.save()
+        # username = form.cleaned_data['username']
+        # password = form.cleaned_data['password']
+        # user = authenticate(username=username, password=password)
+        # login(request, user)
 
     context = {'form': form}
     return render(request, 'form.html', context)
